4bce41155e658f8e135407fac34d5ae8bdf27384
AzadKshitij <azadkshitij08302001@gmail.com>
2020-08-02 02:08:49 +0530

    02-08-2020, 02:08

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        Debug.Log(collision.name);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 100.0f;
+
+    private Rigidbody2D playerRB;
+    private Vector3 moveDir;
+    private bool isDashing = false;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = 0.0f;
+        float moveY = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            moveX = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            moveX = 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDashing = true;
+        }
+
+        moveDir = new Vector3(moveX, moveY, 0).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        float dashAmount = 2.0f;
+        playerRB.velocity = moveDir * speed * Time.deltaTime;
+        if(isDashing)
+        {
+            playerRB.MovePosition(transform.position + moveDir * dashAmount);
+            isDashing = false;
+        }
+    }
+}
